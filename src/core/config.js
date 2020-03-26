1ea194b315c3f3ca817ed06461de3f7128882096
Ayinde Castro <ayindecastro10@gmail.com>
2020-03-26 14:51:20 -0700

    first commit

---
+++ src/core/config.js
@@ -0,0 +1,12 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCeWi5U5c4U_sDQz08GeHKNIhmcLSkq0ZY",
+    authDomain: "flex-78875.firebaseapp.com",
+    databaseURL: "https://flex-78875.firebaseio.com",
+    projectId: "flex-78875",
+    storageBucket: "flex-78875.appspot.com",
+    messagingSenderId: "1018037864183",
+    appId: "1:1018037864183:web:83819233ded476a7a635f2",
+    measurementId: "G-2TV894VHPV"
+
+};
