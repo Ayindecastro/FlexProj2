2b7dd4fff1ee0e67767f8329ffbaa260b012211e
Alex-Bishka <abishka@g.hmc.edu>
2020-04-04 03:31:51 -0500

    feat(MyProfileScreen.js): added functionality to wallet button

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -1,15 +1,107 @@
 import React, { memo } from "react";
-import ProfileBackground from "../components/ProfileBackground";
-import Header from "../components/Header";
-import BackButton from "../components/BackButton";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import Wallet from "../components/ButtonWallet";
+import Background from "../components/Background";
+import { screenWidth } from "../core/dimensions";
 
 
-// Default look for the profile screen
+// Profile screen
 const MyProfileScreen = ({ navigation }) => (
-    <ProfileBackground>
-      <BackButton goBack={() => navigation.navigate("HomeScreen")} unsubscribe/>
-      <Header>Your Profile</Header>
-    </ProfileBackground>
+    // <Background>
+      
+        <View style={[styles.container, styles.materialCardWithButtons]}>
+          {/* background image*/}
+          <Image
+            source={require("../assets/sampleImage.png")}
+            style={styles.cardItemImagePlace}
+          ></Image>
+          {/* buttons for lower panel*/}
+          <View style={styles.buttonGroup}>
+
+            {/* button to trading screen */}
+            <Wallet  
+              onWalletPress = {() => navigation.navigate("TradingScreen")}
+              outerComponentStyle = {styles.leftBtn} 
+              innerComponentStyle = {styles.icon1} />
+
+            {/* button friends search bar */}
+            <TouchableOpacity style={styles.centerBtn}>
+              <MaterialCommunityIconsIcon
+                name="account-group"
+                style={styles.icon2}
+              ></MaterialCommunityIconsIcon>
+            </TouchableOpacity>
+
+            {/* button to edit his dining preferences,
+              trading preferences, etc. */}
+            <TouchableOpacity style={styles.rightBtn}>
+              <MaterialCommunityIconsIcon
+                name="cogs"
+                style={styles.icon3}
+              ></MaterialCommunityIconsIcon>
+            </TouchableOpacity>
+            
+          </View>
+        </View>
+      
+    // </Background>
   );
 
+
+const styles = StyleSheet.create({
+  container: {
+    width: screenWidth,
+    flex: 1,
+    backgroundColor: "#FFF",
+    flexWrap: "nowrap",
+    elevation: 3,
+    shadowOffset: {
+      height: 2,
+      width: -2,
+    },
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 1.5,
+    overflow: "hidden",
+  },
+  cardItemImagePlace: {
+    flex: 1,
+    minHeight: 180,
+    width: screenWidth,
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 8,
+  },
+  leftBtn: {
+    padding: 8,
+  },
+  icon1: {
+    fontSize: 24,
+    color: "#000",
+    opacity: 0.5,
+  },
+  centerBtn: {
+    padding: 8,
+  },
+  icon2: {
+    fontSize: 24,
+    color: "#000",
+    opacity: 0.5,
+  },
+  rightBtn: {
+    padding: 8,
+  },
+  icon3: {
+    fontSize: 24,
+    color: "#000",
+    opacity: 0.5,
+  }
+});
+
 export default memo(MyProfileScreen);
+
+
+
