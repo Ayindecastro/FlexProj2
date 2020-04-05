2e38d01a2140a58900ecd2c76ff95a524f95babd
Alex-Bishka <abishka@g.hmc.edu>
2020-04-04 22:38:06 -0500

    feat(MyProfileScreen): added simple profile picture option

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -1,50 +1,66 @@
 import React, { memo } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, ImageBackground, Button} from "react-native";
 import Wallet from "../components/ButtonWallet";
 import Friends from "../components/FriendsButton";
 import Settings from "../components/SettingsButton";
-import Back from "../components/BackButton";
+import BackButton from "../components/BackButton";
 import { screenWidth } from "../core/dimensions";
 
 
+const profileWidth= screenWidth * 0.5;
+console.log(profileWidth);
+
 // Profile screen
 const MyProfileScreen = ({ navigation }) => (
-      <View>
-        {/* return to home screen 
-        note - currently does not appear on screen*/}
-        <BackButton goBack = {() => navigation.navigate("HomeScreen")}/>
+  
+  // {/* Profile Screen's panel conglomeration of components */}
+  <View style={[styles.container]}>    
+    
 
-        {/* Profile Screen's panel conglomeration of components */}
-        <View style={[styles.container, styles.materialCardWithButtons]}>
-          {/* background image*/}
-          <Image
-            source={require("../assets/sampleImage.png")}
-            style={styles.cardItemImagePlace}
-          ></Image>
-          {/* buttons for lower panel*/}
-          <View style={styles.buttonGroup}>
+    {/* background image*/}
+    <ImageBackground
+      source={require("../assets/sampleImage.png")}
+      style={styles.cardItemImagePlace}/>
 
-            {/* button to trading screen */}
-            <Wallet  
-              onWalletPress = {() => navigation.navigate("TradingScreen")}
-              outerComponentStyle = {styles.leftBtn} 
-              innerComponentStyle = {styles.icon1} />
+    
+    {/* buttons for lower panel*/}
+    <View style={styles.buttonGroup}>
 
-            {/* button friends search bar */}
-            <Friends
-              onFriendPress = {() => navigation.navigate("FriendScreen")}
-              outerComponentStyle = {styles.centerBtn}
-              innerComponentStyle = {styles.icon2} />
+      {/* button to trading screen */}
+      <Wallet  
+        onWalletPress = {() => navigation.navigate("TradingScreen")}
+        outerComponentStyle = {styles.leftBtn} 
+        innerComponentStyle = {styles.icon1} />
 
-            {/* button to edit his dining preferences,
-              trading preferences, etc. */}
-            <Settings
-              onSettingsPress = {() => navigation.navigate("SettingsScreen")}
-              outerComponentStyle = {styles.rightBtn}
-              innerComponentStyle = {styles.icon3} />            
-          </View>
+      {/* button friends search bar */}
+      <Friends
+        onFriendPress = {() => navigation.navigate("FriendScreen")}
+        outerComponentStyle = {styles.centerBtn}
+        innerComponentStyle = {styles.icon2} />
+
+      {/* button to edit his dining preferences,
+        trading preferences, etc. */}
+      <Settings
+        onSettingsPress = {() => navigation.navigate("SettingsScreen")}
+        outerComponentStyle = {styles.rightBtn}
+        innerComponentStyle = {styles.icon3} />            
+    </View>
+
+    <View style = {styles.defaultProPicView}>
+      <ImageBackground 
+        source = {require("../assets/default_profile.png")}
+        style = {styles.profilePic}>
+        <View style = {styles.editButtonView}>
+          <Button 
+            title= "Edit Profile Picture"/>
         </View>
-      </View>
+      </ImageBackground>
+    </View>
+
+    {/* button back to home
+    needs to be after everything else in order to overlay with position = 'absolute' */}
+    <BackButton style = {styles.backbutton} goBack = {() => navigation.navigate("HomeScreen")}/>
+  </View>
   );
 
 
@@ -52,22 +68,36 @@ const styles = StyleSheet.create({
   container: {
     width: screenWidth,
     flex: 1,
-    backgroundColor: "#FFF",
-    flexWrap: "nowrap",
-    elevation: 3,
-    shadowOffset: {
-      height: 2,
-      width: -2,
-    },
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 1.5,
-    overflow: "hidden",
   },
   cardItemImagePlace: {
-    flex: 1,
-    minHeight: 180,
+    flex: 3,
     width: screenWidth,
+  },
+  backbuttonView: {
+    position: "absolute",
+  },
+  defaultProPicView: {
+    position: "absolute",
+    // width: 200,
+    // height: 200,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    // backgroundColor: "white",
+  },
+  profilePic: {
+    width: 200,
+    height: 200,
+  },
+  editButtonView: {
+    position: "absolute",
+    justifyContent: "center",
+    backgroundColor: "white",
+    color: "black",
+    bottom: 0,
   },
   buttonGroup: {
     flexDirection: "row",
