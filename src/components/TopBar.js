8b34419b4fc131164fefdf3b4d8813e27b95811d
Alex-Bishka <abishka@g.hmc.edu>
2020-04-07 14:42:30 -0500

    feat(TopBar.js): added style specifically to the bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,24 @@
 import React, { memo } from "react";
 import { View } from "react-native";
+import { StyleSheet } from "react-native";
+import { screenWidth, screenHeight } from "../core/dimensions";
 
 
 const topBar = ({topBarStyle}) => (
-    <View style = {topBarStyle}/>
+    <View style = {styles.containerTopBar}/>
 )
+
+
+const styles = StyleSheet.create({
+    containerTopBar: {
+        backgroundColor: 'white',
+        top: 0,
+        width: screenWidth,
+        height: '2.69%', 
+        // height: 18,
+        position: 'absolute',
+    }
+});
 
 export default memo(topBar);
 
