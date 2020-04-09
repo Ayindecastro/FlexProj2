d27c645a4731cf2b150fe65cf8751ecccf22f036
Alex-Bishka <abishka@g.hmc.edu>
2020-04-09 18:25:17 -0500

    feat(switchComp): created reuseable switch

--- src/screens/TradingScreen.js
+++ src/screens/TradingScreen.js
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import Background from "../components/Background";
 import Header from "../components/Header";
 import BackButton from "../components/BackButton";
+import SwitchComp from "../components/SwitchComp"; 
 
 
 // Default look for screen
@@ -9,6 +10,7 @@ const TradingScreen = ({ navigation }) => (
     <Background>
       <BackButton goBack={() => navigation.navigate("MyProfileScreen")}/>
       <Header>Trade Screen</Header>
+      <SwitchComp/>
     </Background>
   );
 
